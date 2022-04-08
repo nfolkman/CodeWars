8c96294c22cb6e5e50887151523768c487838a17
Nathan Folkman <98185555+nfolkman@users.noreply.github.com>
2022-04-08 00:54:42 -0500

    Update challenge26.js

--- challenge26.js
+++ challenge26.js
@@ -1,3 +1,11 @@
+/* Challenge : Your task is to create the functionisDivideBy (or is_divide_by) to check if an integer number is divisible by both integers a and b.
+
+    A few cases:
+    (-12, 2, -6)  ->  true
+    (-12, 2, -5)  ->  false*/
+
+
+
 function isDivideBy(number, a, b) {
     if ( number % a == 0 && number % b == 0) {
       return true
