83c060e2753c8b2a0e59f329d301ebc928a2981e
Nathan Folkman <98185555+nfolkman@users.noreply.github.com>
2022-04-14 00:12:48 -0500

    challenge34

---
+++ challenge34.js
@@ -0,0 +1,10 @@
+/* Description: Write a function to split a string and convert it into an array of words.
+
+Examples (Input -> Output):
+* "Robin Singh" ==> ["Robin", "Singh"] */
+
+
+function stringToArray(string){
+
+    return string.split(' ').concat()
+  }
