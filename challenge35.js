cb275caa5a36b5cae52f900eaf0eb349ebb8d722
Nathan Folkman <98185555+nfolkman@users.noreply.github.com>
2022-04-15 18:50:45 -0500

    challenge35

---
+++ challenge35.js
@@ -0,0 +1,26 @@
+/* Description: 
+Kids drink toddy.
+Teens drink coke.
+Young adults drink beer.
+Adults drink whisky.
+Make a function that receive age, and return what they drink.
+
+Rules:
+
+Children under 14 old.
+Teens under 18 old.
+Young under 21 old.
+Adults have 21 or more.  */
+
+
+function peopleWithAgeDrink(age) {
+	if (age < 14) {
+		return 'drink toddy'
+	} else if (age < 18) {
+		return 'drink coke'
+	} else if (age < 21) {
+		return 'drink beer'
+	} else {
+	return 'drink whisky'
+}
+}
