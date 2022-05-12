8a33137daacb90f1335df21ce60326bac989408d
Nathan Folkman <98185555+nfolkman@users.noreply.github.com>
2022-05-12 01:44:15 -0500

    challenge 63 - 7 kyu

---
+++ challenge63.js
@@ -0,0 +1,9 @@
+/* Description: Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.  */
+
+
+function findShort(s){
+    s = s.split(' ');
+    return s.map((el)=> el.length).sort((a,z)=> a-z)[0]
+}
