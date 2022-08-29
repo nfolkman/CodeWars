8e3cbb15a6b8d6df767a3e37822c6ae9ed75d025
Nathan Folkman <98185555+nfolkman@users.noreply.github.com>
2022-08-28 19:16:42 -0500

    8 kyu - Data Types & Arithmetic

---
+++ Super Duper Easy.js
@@ -0,0 +1,14 @@
+/* Description: Make a function that returns the value multiplied by 50 and increased by 6. 
+If the value entered is a string it should return "Error".*/
+
+
+function problem(x){
+    if(typeof(x) != 'number') return 'Error'
+  
+    return (x*50)+6
+}
+
+// test cases:
+problem(1)
+problem(15)
+problem('hello')
