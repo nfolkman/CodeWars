1f52167a23fbed093f2d7154c387aa21dd453459
Nathan Folkman <98185555+nfolkman@users.noreply.github.com>
2023-02-14 11:34:03 -0800

    7 kyu - Fundamentals / Functional Programming / Algorithms

---
+++ Coding-Meeting-11.js
@@ -0,0 +1,25 @@
+/* Description: 
+You will be given a sequence of objects representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+Given the following input array:
+
+var list1 = [
+  { firstName: 'Maria', lastName: 'Y.', country: 'Cyprus', continent: 'Europe', age: 30, language: 'Java' },
+  { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 70, language: 'Python' },
+];
+write a function that returns the average age of developers (rounded to the nearest integer). In the example above your function should return 50 (number).
+
+Notes:
+
+The input array will always be valid and formatted as in the example above.
+Age is represented by a number which can be any positive integer.   */
+
+
+function getAverageAge(list) {
+    let age = 0
+    for(let i = 0; i< list.length; i++){
+      age += list[i].age
+    }
+    let avrg = age/list.length
+    return Math.round(avrg)
+  }
